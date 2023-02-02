31d964b5c57f52e4fb9f5be715c30f95f84bd5c9
Yichao Wu <wuyichao71@126.com>
2023-02-02 08:00:10 +0800

    update to lab06

---
+++ disc/disc05/map_filter_reduce.py
@@ -0,0 +1,43 @@
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    """
+    "*** YOUR CODE HERE ***"
+    return [fn(i) for i in seq]
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    return [i for i in seq if pred(i)]
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    result = seq[0]
+    for i in seq[1:]:
+        result = combiner(result, i)
+    return result
+
+def count_palindromes(L):
+    """The number of palindromic strings in the sequence of strings
+    L (ignoring case).
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    >>> count_palindromes(["101", "rAcECaR", "much", "wow"])
+    3
+    """
+    return my_reduce(lambda x, y: x+y, my_map(lambda s: 1, my_filter(lambda s: s == s[::-1], my_map(lambda s: s.lower(), L))))
