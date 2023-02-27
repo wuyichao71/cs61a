4474926a4d2b3a8344400acb8d601349d0691c49
hapo <DZ1722034@smail.nju.edu.cn>
2023-02-27 16:07:18 +0800

    start lab10

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -5,10 +5,12 @@
 (define (caddr s) (car (cddr s)))
 
 (define (ascending? asc-lst) 
-  (cond
-    ((null? (cdr asc-lst)) #t)
-    ((> (car asc-lst) (cadr asc-lst)) #f)
-    (else (ascending? (cdr asc-lst))))
+;   (cond
+;     ((null? (cdr asc-lst)) #t)
+;     ((> (car asc-lst) (cadr asc-lst)) #f)
+;     (else (ascending? (cdr asc-lst))))
+  (if (null? (cdr asc-lst)) #t
+    (and (<= (car asc-lst) (cadr asc-lst)) (ascending? (cdr asc-lst))))
 )
 
 
@@ -16,7 +18,7 @@
 
 (define (pow base exp) 
     (cond
-        ((= exp 1) base)
+        ((= exp 0) 1)
         ((even? exp) (square (pow base (quotient exp 2))))
         ((odd? exp) (* base (square (pow base (quotient exp 2)))))
     )
