cb5e7a052387b3c97b3df63676308fa7961ea1e3
Yichao Wu <wuyichao71@126.com>
2023-03-04 14:51:33 +0800

    hw08 Q2

--- homework/hw08/hw08.scm
+++ homework/hw08/hw08.scm
@@ -4,7 +4,11 @@
    ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
    (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (cond
+   ((null? lst1) lst2)
+   ((null? lst2) lst1)
+   (else (cons (car lst1) (interleave lst2 (cdr lst1))))))
 
 (define (accumulate joiner start n term)
   'YOUR-CODE-HERE)
