d9a0eb39bc894d10d9a90be77ff725f2babc3d8d
Yichao Wu <wuyichao71@126.com>
2023-03-04 13:05:24 +0800

    hw08 Q1

--- homework/hw08/hw08.scm
+++ homework/hw08/hw08.scm
@@ -1,4 +1,8 @@
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (cond
+   ((null? s) s)
+   ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+   (else (my-filter pred (cdr s)))))
 
 (define (interleave lst1 lst2) 'YOUR-CODE-HERE)
 
