c5eab6a6d78644d6496c0f9a2bcfb7df611b3ecd
Yichao Wu <wuyichao71@126.com>
2023-03-08 02:15:03 +0800

    project4 problem6

--- projects/scheme/scheme_eval_apply.py
+++ projects/scheme/scheme_eval_apply.py
@@ -93,7 +93,13 @@ def eval_all(expressions, env):
     2
     """
     # BEGIN PROBLEM 6
-    return scheme_eval(expressions.first, env)  # replace this with lines of your own code
+    # return scheme_eval(expressions.first, env)  # replace this with lines of your own code
+    if expressions is nil:
+        return None
+    while expressions.rest is not nil:
+        scheme_eval(expressions.first, env)
+        expressions = expressions.rest
+    return scheme_eval(expressions.first, env)
     # END PROBLEM 6
 
 
